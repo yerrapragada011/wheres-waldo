3fba251e57ce448b6f4a0132fd944b896a70269b
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-09-13 15:32:11 -0400

    Set cors headers

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,13 @@ app.use(
   })
 )
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://wheres-waldo-mu.vercel.app'
+  )
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
 
