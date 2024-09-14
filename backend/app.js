4c7ac0546f1e0d3d7bad02a154955c80aab851be
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-09-14 09:29:46 -0400

    Update cors config

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -8,25 +7,31 @@ const app = express()
 
 app.use(express.json())
 app.use(bodyParser.json())
-app.options('*', cors())
+
 app.use(
   cors({
     origin: 'https://wheres-waldo-mu.vercel.app',
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: [
       'Content-Type',
       'Authorization',
+      'Access-Control-Allow-Origin',
       'Access-Control-Allow-Methods',
-      'Access-Control-Request-Headers'
+      'Access-Control-Allow-Headers'
     ]
   })
 )
+
+app.use((req, res, next) => {
+  console.log(req.headers)
+  next()
+})
 
 app.get('/api/characters', getCharacters)
 app.post('/api/submit-score', submitScore)
 app.get('/api/leaderboard', leaderboardList)
 
 const PORT = process.env.PORT || 8000
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
